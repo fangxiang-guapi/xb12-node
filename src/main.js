1a4dbc2ea3e53a610d4ed3cfbbf5de7815e43307
zhongjiang <1487006403@qq.com>
2025-03-19 16:25:12 +0800

    http操作

--- src/main.js
+++ src/main.js
@@ -32,6 +32,24 @@ app.get("/posts/:postId", (request, response) => {
   response.send(posts[0]);
 });
 
+/**
+ * 创建内容
+ */
+app.post("/posts", (request, response) => {
+  // 设置响应头部数据
+  response.set("Sing-Along", "How I wonder what you are!");
+  // 获取请求里的数据
+  const { content } = request.body;
+  // 作出响应
+  response.send({
+    message: `成功创建了内容：${content}`,
+  });
+  // 设置响应状态码
+  response.status(201);
+  // 输出请求头部数据
+  console.log(request.headers["sing-along"]);
+});
+
 // 🟢 启动服务器（放在最后）
 app.listen(port, () => {
   console.log(`🚀 服务器已启动，监听端口 ${port}`);
