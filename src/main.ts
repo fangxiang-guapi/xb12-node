25b100866d1c8a9050c62bebd9217a6840640693
zhongjiang <1487006403@qq.com>
2025-03-20 15:19:58 +0800

    客户端到服务器与数据库的相关操作

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import app from './app';
 import { APP_PORT } from './app/app.config';
+import { connection } from './app/database/mysql';
 
 // 🟢 启动服务器（放在最后）
 app.listen(APP_PORT, () => {
   console.log(`🚀 服务器已启动，监听端口 ${APP_PORT}`);
+});
+/** 
+* 测试使用数据服务连接 
+*/ 
+connection.connect(error => { 
+  if (error) { 
+  console.log(' �连接数据服务失败：', error.message); 
+  return; 
+  }
+  console.log('�成功连接数据服务 ~~'); 
 });
